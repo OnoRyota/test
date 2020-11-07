d93e4fc103357b813cda44589d500fda8d79ad00
OnoRyota <OnoRyota@users.noreply.github.com>
2020-11-07 10:33:21 +0000

    deploy: a6532fb004939bc45be97022704868c4d0eb4b45

--- DataFrames.py
+++ DataFrames.py
@@ -1,7 +1,10 @@
 import pandas #pandasをインポート
 import pprint
 
-#取得したいページのurlを入力
+import requests
+from bs4 import BeautifulSoup
+
+#取得したいページのurlを入力(url)
 url = 'https://www.jma.go.jp/jp/amedas_h/today-83216.html?areaCode=000&groupCode=59'
 
 #urlを読み取る
@@ -11,5 +14,15 @@ fetched_daraframes = pandas.io.html.read_html(url)
 fetched_daraframes[4].to_json('OitaNoAme.json',force_ascii=False)
 
 #プリントしたい場合
-ret = fetched_daraframes[4].to_json(force_ascii=False)
-pprint.pprint(ret)
+#ret = fetched_daraframes[4].to_json(force_ascii=False)
+#pprint.pprint(ret)
+
+#日付取得
+html = requests.get(url).content
+soup = BeautifulSoup(html, 'html.parser')
+date = soup.find(class_ = "td_title height2").get_text()
+
+#日付をテキストファイル(date.txt)に出力
+file = open('date.txt','w')
+file.write(date)
+file.close()
