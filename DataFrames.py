d43d0e571e440bada09bdb244e84d8dafb9911ec
OnoRyota <OnoRyota@users.noreply.github.com>
2021-01-11 19:47:25 +0000

    deploy: 136b2dd4052d96b5c1d7b1bec1a772df3db03b90

--- DataFrames.py
+++ DataFrames.py
@@ -5,24 +5,15 @@ import requests
 from bs4 import BeautifulSoup
 
 #取得したいページのurlを入力(url)
-url = 'https://www.jma.go.jp/jp/amedas_h/today-83216.html?areaCode=000&groupCode=59'
+url = 'https://weather.yahoo.co.jp/weather/jp/44/8320/44214.html'
 
 #urlを読み取る
-fetched_daraframes = pandas.io.html.read_html(url)
+fetched_dataframes = pandas.io.html.read_html(url)
 
-#to_json関数で文字列をjsonに変換し，OitaAmeme.jsonで保存
-fetched_daraframes[4].to_json('OitaNoAme.json',force_ascii=False)
+#to_json関数で文字列をjsonに変換し，checkitup.jsonで保存
+#[4]で明日の天気,[5]で一週間の天気
+fetched_dataframes[4].to_json('check.json',force_ascii=False)
 
 #プリントしたい場合
-#ret = fetched_daraframes[4].to_json(force_ascii=False)
-#pprint.pprint(ret)
-
-#日付取得
-html = requests.get(url).content
-soup = BeautifulSoup(html, 'html.parser')
-date = soup.find(class_ = "td_title height2").get_text()
-
-#日付をテキストファイル(date.txt)に出力
-file = open('date.txt','w')
-file.write(date)
-file.close()
+ret = fetched_dataframes[4].to_json(force_ascii=False)
+pprint.pprint(ret)
